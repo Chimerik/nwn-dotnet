7e50a139ebba92b7413ff8506050f48bc6a99134
RGR <rg.robingranger@gmail.com>
2020-05-10 16:58:51 +0200

    initial commit

---
+++ ScriptHandlers/ScriptHandlers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWN
+{
+    class ScriptHandlers
+    {
+        public static Dictionary<string, Func<uint, int>> Register = new Dictionary<string, Func<uint, int>>
+        {
+            { "craft_onatk", Craft_OnAtk },
+        };
+
+        private static int Craft_OnAtk(uint oidSelf)
+        {
+            var objName = NWScript.GetName(oidSelf);
+            var oPC = NWScript.GetLastAttacker(oidSelf);
+            var pcName = NWScript.GetName(oPC);
+            NWScript.SendMessageToPC(oPC, $"{pcName} is attacking a poor {objName}");
+            return Entrypoints.SCRIPT_HANDLED;
+        }
+    }
+}
