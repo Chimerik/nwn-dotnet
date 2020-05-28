64bea330c66fae44783e9c77970448aec3a494e0
RGR <rg.robingranger@gmail.com>
2020-05-28 16:45:36 +0200

    chore(del) : del some useless stuff

--- ScriptHandlers/ScriptHandlers.cs
+++ ScriptHandlers/ScriptHandlers.cs
@@ -1,5 +1,4 @@
 ﻿using NWN.Enums;
-using NWN.Enums.Creature;
 using NWN.Enums.Item;
 using NWN.Enums.Item.Property;
 using NWN.Enums.VisualEffect;
@@ -7,9 +6,7 @@ using NWN.NWNX;
 using NWN.Systems.PostString;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Linq;
-using System.Timers;
 
 namespace NWN
 {
@@ -17,8 +14,7 @@ namespace NWN
     {
         public static Dictionary<string, Func<uint, int>> Register = new Dictionary<string, Func<uint, int>>
         {
-            { "_onload", OnModuleLoad }, // le OnLoad de CdE s'appelle _onload. Du coup, pour l'instant renomme le 
-            { "craft_onatk", Craft_OnAtk },
+            { "_onload", OnModuleLoad },
             { "cs_chatlistener", ChatListener },
             { "event_keyboard", EventKeyboard },
             { "X0_S0_AcidSplash", CantripsScaler },
@@ -32,15 +28,6 @@ namespace NWN
             { "event_mouse_clic", EventMouseClick },
         }.Concat(Systems.Loot.Register)
          .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-
-        private static int Craft_OnAtk(uint oidSelf)
-        {
-            var objName = NWScript.GetName(oidSelf);
-            var oPC = NWScript.GetLastAttacker(oidSelf);
-            var pcName = NWScript.GetName(oPC);
-            NWScript.SendMessageToPC(oPC, $"{pcName} is attacking a poor {objName}");
-            return Entrypoints.SCRIPT_HANDLED;
-        }
 
         private static int OnModuleLoad (uint oidSelf)
         {
