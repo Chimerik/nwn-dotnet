523a2836ad86c04408a53d56d0bea049f3c6349e
Chim <chimaera.no.saki@gmail.com>
2020-07-13 15:55:32 +0200

    Méga nettoyage pour être synchro avec ta version
    A partir de là, j'essayerai de faire des petites branches par fonctionnalité (je dis bien : essayerai :D)

--- ScriptHandlers/ActivateItemHandlers.cs
+++ ScriptHandlers/ActivateItemHandlers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using NWN.Systems;
 
 namespace NWN.ScriptHandlers
 {
@@ -8,10 +9,22 @@ namespace NWN.ScriptHandlers
     public static Dictionary<string, Func<uint, uint, int>> Register = new Dictionary<string, Func<uint, uint, int>>
         {
             { "MenuTester", HandleMenuTesterActivate },
+            { "test_block", HandleBockTesterActivate },
         };
     private static int HandleMenuTesterActivate(uint oItem, uint oActivator)
     {
       Console.WriteLine($"You activated the item {NWScript.GetName(oItem)}! {NWScript.GetName(oActivator)}");
+
+      return Entrypoints.SCRIPT_HANDLED;
+    }
+
+    private static int HandleBockTesterActivate(uint oItem, uint oActivator)
+    {
+      PlayerSystem.Player player;
+      if (PlayerSystem.Players.TryGetValue(oActivator, out player))
+      {
+        player.BlockPlayer();
+      }
 
       return Entrypoints.SCRIPT_HANDLED;
     }
---
+++ ScriptHandlers/ChatHandlers.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using NWN.Enums;
+using NWN.NWNX;
+using NWN.Systems;
+using NWN.Systems.PostString;
+
+namespace NWN.ScriptHandlers
+{
+  static public class ChatHandlers
+  {
+    public static Dictionary<string, Func<string, NWPlayer, uint, ChatChannel, int>> Register = new Dictionary<string, Func<string, NWPlayer, uint, ChatChannel, int>>
+    {
+            { "walk", HandleWalkCommand },
+            { "testmenu", HandleTestMenuCommand },
+            { "testblockdotnet", HandleTestBlockCommand },
+            { "frostattack", HandleFrostAttackCommand },
+    };
+
+    private static int HandleWalkCommand(string sChatReceived, NWPlayer oChatSender, uint oChatTarget, ChatChannel iChannel)
+    {
+      Chat.SkipMessage();
+      if (NWNX.Object.GetInt(oChatSender, "_ALWAYS_WALK") == 0)
+      {
+        NWNX.Player.SetAlwaysWalk(oChatSender, true);
+        NWNX.Object.SetInt(oChatSender, "_ALWAYS_WALK", 1, true);
+        oChatSender.SendMessage("Vous avez activé le mode marche.");
+      }
+      else
+      {
+        NWNX.Player.SetAlwaysWalk(oChatSender, false);
+        NWNX.Object.DeleteInt(oChatSender, "_ALWAYS_WALK");
+        oChatSender.SendMessage("Vous avez désactivé le mode marche.");
+      }
+
+      return Entrypoints.SCRIPT_HANDLED;
+    }
+    private static int HandleTestMenuCommand(string sChatReceived, NWPlayer oChatSender, uint oChatTarget, ChatChannel iChannel)
+    {
+      Chat.SkipMessage();
+      NWScript.SetLocalInt(oChatSender, "_MENU_ON", 1);
+      PostString.Menu_UpdateGUI(oChatSender);
+      PostString.Menu_DrawStaticGUI(oChatSender);
+      return Entrypoints.SCRIPT_HANDLED;
+    }
+    private static int HandleTestBlockCommand(string sChatReceived, NWPlayer oChatSender, uint oChatTarget, ChatChannel iChannel)
+    {
+      Chat.SkipMessage();
+      PlayerSystem.Player oPC;
+      if (PlayerSystem.Players.TryGetValue(oChatSender, out oPC))
+      {
+        oPC.BlockPlayer();
+      }
+      return Entrypoints.SCRIPT_HANDLED;
+    }
+    private static int HandleFrostAttackCommand(string sChatReceived, NWPlayer oChatSender, uint oChatTarget, ChatChannel iChannel)
+    {
+      PlayerSystem.Player oPC;
+      if (PlayerSystem.Players.TryGetValue(oChatSender, out oPC))
+      {
+        if (sChatReceived.StartsWith("!frostattack"))
+        {
+          Chat.SkipMessage();
+
+          if (oChatSender.HasSpell(Spell.RayOfFrost))
+          {
+            if (NWNX.Object.GetInt(oChatSender, "_FROST_ATTACK") == 0)
+            {
+              NWNX.Object.SetInt(oChatSender, "_FROST_ATTACK", 1, true);
+              NWNX.Events.AddObjectToDispatchList("NWNX_ON_INPUT_ATTACK_OBJECT_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.AddObjectToDispatchList("NWNX_ON_INPUT_FORCE_MOVE_TO_OBJECT_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.AddObjectToDispatchList("NWNX_ON_INPUT_CAST_SPELL_BEFORE", "_onspellcast", oChatSender);
+              NWNX.Events.AddObjectToDispatchList("NWNX_ON_INPUT_KEYBOARD_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.AddObjectToDispatchList("NWNX_ON_INPUT_WALK_TO_WAYPOINT_BEFORE", "event_auto_spell", oChatSender);
+              oPC.SendMessage("Vous activez le mode d'attaque par rayon de froid");
+            }
+            else
+            {
+              NWNX.Object.DeleteInt(oChatSender, "_FROST_ATTACK");
+              NWNX.Events.RemoveObjectFromDispatchList("NWNX_ON_INPUT_ATTACK_OBJECT_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.RemoveObjectFromDispatchList("NWNX_ON_INPUT_FORCE_MOVE_TO_OBJECT_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.RemoveObjectFromDispatchList("NWNX_ON_INPUT_CAST_SPELL_BEFORE", "_onspellcast", oChatSender);
+              NWNX.Events.RemoveObjectFromDispatchList("NWNX_ON_INPUT_KEYBOARD_BEFORE", "event_auto_spell", oChatSender);
+              NWNX.Events.RemoveObjectFromDispatchList("NWNX_ON_INPUT_WALK_TO_WAYPOINT_BEFORE", "event_auto_spell", oChatSender);
+              oPC.SendMessage("Vous désactivez le mode d'attaque par rayon de froid");
+            }
+          }
+          else
+            oPC.SendMessage("Il vous faut pouvoir lancer le sort rayon de froid pour activer ce mode.");
+
+          
+        }
+      }
+
+      return Entrypoints.SCRIPT_HANDLED;
+    }
+  } 
+}
