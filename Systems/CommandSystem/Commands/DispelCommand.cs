7a506f8d37cb9607a1de2a191ad8fae5193cef37
Chim <chimaera.no.saki@gmail.com>
2020-07-15 16:33:33 +0200

    Ajout commandes sans arguments
    En attente de la possibilité d'ajouter les commandes avec arguments !

---
+++ Systems/CommandSystem/Commands/DispelCommand.cs
@@ -0,0 +1,28 @@
+﻿using NWN.Enums;
+using NWN.NWNX;
+
+namespace NWN.Systems
+{
+  public static partial class CommandSystem
+  {
+    private static void ExecuteDispelCommand(ChatSystem.ChatEventArgs e)
+    {
+      if (NWScript.GetIsObjectValid(e.oTarget))
+      {
+        foreach (Effect eff in e.oTarget.Effects)
+        {
+          if (NWScript.GetEffectCreator(eff) == e.oSender && NWScript.GetEffectTag(eff) == "")
+            NWScript.RemoveEffect(e.oTarget, eff);
+        }
+      }
+      else
+      {
+        foreach (Effect eff in e.oSender.Effects)
+        {
+          if (NWScript.GetEffectCreator(eff) == e.oSender && NWScript.GetEffectTag(eff) == "")
+            NWScript.RemoveEffect(e.oSender, eff);
+        }
+      }
+    }
+  }
+}
