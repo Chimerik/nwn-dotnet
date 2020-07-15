307110f83e2656eff48c00fc192a448ea5edbc0d
Robin Granger <rg.robingranger@gmail.com>
2020-07-15 18:53:44 +0200

    wip

---
+++ Systems/CommandSystem/Options/Option.cs
@@ -0,0 +1,21 @@
+﻿namespace NWN.Systems
+{
+  public static partial class CommandSystem
+  {
+    private class Option
+    {
+      string name { get; }
+      string alias { get; }
+      string description { get; }
+      string value { get; set; }
+
+      Option (string description, string defaultValue = null, string name = null, string alias = null)
+      {
+        this.description = description;
+        this.name = name;
+        this.alias = alias;
+        this.value = defaultValue;
+      }
+    }
+  }
+}
---
+++ Systems/CommandSystem/Options/Options.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace NWN.Systems
+{
+  public static partial class CommandSystem
+  {
+    private class Options
+    {
+      List<Option> positional;
+      Dictionary<string, Option> named;
+
+      public Options (List<Option> positional, Dictionary<string, Option> named)
+      {
+        this.positional ??= new List<Option>();
+        this.named ??= new Dictionary<string, Option>();
+      }
+
+      void Parse (string args)
+      {
+        // TODO
+      }
+    }
+  }
+}
