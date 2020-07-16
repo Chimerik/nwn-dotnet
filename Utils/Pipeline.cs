dd04e7e5b843d8f788818a6838dc93a4da5aacc5
Robin Granger <rg.robingranger@gmail.com>
2020-07-16 20:11:08 +0200

    feature(utils) : ajoute une classe utilitaire Pipeline

---
+++ Utils/Pipeline.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace NWN
+{
+  public class Pipeline<T>
+  {
+    private T currentContext;
+    private int currentIndex = 0;
+    private Action<T, Action>[] middlewares;
+
+    public Pipeline(Action<T, Action>[] middlewares)
+    {
+      this.middlewares = middlewares;
+    }
+
+    public void Execute(T context)
+    {
+      currentContext = context;
+      currentIndex = 0;
+      Next();
+    }
+
+    private void Next()
+    {
+      var middleware = middlewares.ElementAtOrDefault(currentIndex);
+      currentIndex++;
+      middleware?.Invoke(currentContext, Next);
+    }
+  }
+}
