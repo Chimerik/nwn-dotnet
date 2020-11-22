d98c97aec6cec577454518cee1084f963cc13028
RGR <rg.robingranger@gmail.com>
2020-11-22 17:01:27 +0100

    feature(config) : ajoute l'environnement utilisé pour lancer le serveur : production ou development

---
+++ Systems/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NWN.Systems
+{
+  public static class Config
+  {
+    public enum Env
+    {
+      Dev,
+      Prod
+    }
+
+    public static Env env = InitEnv();
+
+    private static Env InitEnv ()
+    {
+      var env = Environment.GetEnvironmentVariable("ENV");
+
+      switch (env)
+      {
+        default: return Env.Prod;
+
+        case "production": return Env.Prod;
+        case "development": return Env.Dev;
+      }
+    }
+  }
+}
