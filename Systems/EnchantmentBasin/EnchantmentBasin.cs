598e95a65fb993e975489408d18e5d61d0d5c15c
RGR <rg.robingranger@gmail.com>
2020-11-28 18:47:06 +0100

    evolution(enchantment-basin) : corrige quelques bugs et ameliore la feature

--- Systems/EnchantmentBasin/EnchantmentBasin.cs
+++ Systems/EnchantmentBasin/EnchantmentBasin.cs
@@ -7,41 +7,56 @@ namespace NWN.Systems
   {
     private class EnchantmentBasin
     {
-      private PlayerSystem.Player player;
-      private uint oItem;
-
       private float costRate;
       private int minSuccessPercent;
       private int maxSuccessPercent;
       private int maxCostRateForSuccessRateMin;
+
+      // Enabled enchantment
+      private bool isAttackBonusEnabled;
+      private bool isACBonusEnabled;
+      private bool isAbilityBonusEnabled;
+      private bool isDamageBonusEnabled;
+      private bool isSavingThrowBonusEnabled;
+      private bool isRegenBonusEnabled;
+
+      // maximum values
       private int maxAttackBonus;
       private int maxACBonus;
       private int maxAbilityBonus;
-      private string maxDamageBonus;
+      private int maxDamageBonus;
       private int maxSavingThrowBonus;
       private int maxRegenBonus;
 
       public EnchantmentBasin(
-        PlayerSystem.Player player,
-        uint oItem,
         float costRate = 1.0f,
         int minSuccessPercent = 1,
         int maxSuccessPercent = 99,
         int maxCostRateForSuccessRateMin = 1000000,
-        int maxAttackBonus = 2,
+        bool isAttackBonusEnabled = true,
+        bool isACBonusEnabled = true,
+        bool isAbilityBonusEnabled = true,
+        bool isDamageBonusEnabled = true,
+        bool isSavingThrowBonusEnabled = true,
+        bool isRegenBonusEnabled = true,
+        int maxAttackBonus = 20,
         int maxACBonus = 20,
         int maxAbilityBonus = 12,
-        string maxDamageBonus = "2d12",
+        int maxDamageBonus = NWScript.DAMAGE_BONUS_20,
         int maxSavingThrowBonus = 20,
         int maxRegenBonus = 20
       )
       {
-        this.player = player;
-        this.oItem = oItem;
         this.costRate = costRate;
         this.minSuccessPercent = minSuccessPercent;
         this.maxSuccessPercent = maxSuccessPercent;
         this.maxCostRateForSuccessRateMin = maxCostRateForSuccessRateMin;
+        this.isAttackBonusEnabled = isAttackBonusEnabled;
+        this.isACBonusEnabled = isACBonusEnabled;
+        this.isAbilityBonusEnabled = isAbilityBonusEnabled;
+        this.isDamageBonusEnabled = isDamageBonusEnabled;
+        this.isSavingThrowBonusEnabled = isSavingThrowBonusEnabled;
+        this.isRegenBonusEnabled = isRegenBonusEnabled;
         this.maxAttackBonus = maxAttackBonus;
         this.maxACBonus = maxACBonus;
         this.maxAbilityBonus = maxAbilityBonus;
@@ -50,119 +65,24 @@ namespace NWN.Systems
         this.maxRegenBonus = maxRegenBonus;
       }
 
-      public void DrawMenu()
+      public void DrawMenu(PlayerSystem.Player player, uint oItem)
       {
         player.menu.Clear();
-        player.menu.title = "Menu du bassin d'enchantement";
+        player.menu.title = "Choisissez un enchantement a appliquer sur votre objet";
 
-        // AB
-        var currentAttackBonus = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ATTACK_BONUS);
-        if (currentAttackBonus < maxAttackBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus d'attaque",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAttackBonus(currentAttackBonus + 1),
-              $"Bonus d'attaque +{currentAttackBonus + 1}"
-            )
-          ));
-        }
+        bool isWeapon = ItemUtils.IsWeapon(oItem);
 
-        // AC
-        var currentAC = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_AC_BONUS);
-        if (currentAC < maxACBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus d'armure",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyACBonus(currentAC+ 1),
-              $"Bonus d'armure +{currentAC + 1}"
-            )
-          ));
-        }
-
-        // STR
-        var currentSTR = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_STRENGTH);
-        if (currentSTR < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus de force",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_STRENGTH, currentAC + 1),
-              $"Bonus de force +{currentSTR + 1}"
-            )
-          ));
-        }
-
-        // DEX
-        var currentDEX = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_DEXTERITY);
-        if (currentDEX < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus de dexterite",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_DEXTERITY, currentDEX + 1),
-              $"Bonus de dexterite +{currentDEX + 1}"
-            )
-          ));
-        }
-
-        // CON
-        var currentCON = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_CONSTITUTION);
-        if (currentCON < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus de constitution",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_CONSTITUTION, currentCON + 1),
-              $"Bonus de constitution +{currentCON + 1}"
-            )
-          ));
-        }
-
-        // INT
-        var currentINT= ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_INTELLIGENCE);
-        if (currentINT < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus d'intelligence",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_INTELLIGENCE, currentINT + 1),
-              $"Bonus d'intelligence +{currentINT + 1}"
-            )
-          ));
-        }
-
-        // CHA
-        var currentCHA = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_CHARISMA);
-        if (currentCHA < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus de charisme",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_CHARISMA, currentCHA + 1),
-              $"Bonus de charisme +{currentCHA + 1}"
-            )
-          ));
-        }
-
-        // WIS
-        var currentWIS = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_WISDOM);
-        if (currentWIS < maxAbilityBonus)
-        {
-          player.menu.choices.Add((
-            "Ajouter bonus de sagesse",
-            () => HandleDrawCost(
-              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_WISDOM, currentWIS + 1),
-              $"Bonus de sagesse +{currentWIS + 1}"
-            )
-          ));
-        }
+        if (isAttackBonusEnabled && isWeapon) AddAttackBonusToMenu(player, oItem);
+        if (isDamageBonusEnabled && isWeapon) AddDamageBonusToMenu(player, oItem);
+        if (isACBonusEnabled) AddACBonusToMenu(player, oItem);
+        if (isAbilityBonusEnabled) AddAbilityBonusToMenu(player, oItem);
+        if (isSavingThrowBonusEnabled) AddSavingThrowBonusToMenu(player, oItem);
+        if (isRegenBonusEnabled) AddRegenBonusToMenu(player, oItem);
 
         player.menu.Draw();
       }
 
-      private void HandleDrawCost(ItemProperty ip, string enchantmentName)
+      private void DrawCostPage(PlayerSystem.Player player, uint oItem, ItemProperty ip, string enchantmentName)
       {
         // Create a copy item to evaluate enchantment cost
         uint oCopy = NWScript.CopyItem(oItem, player.oid);
@@ -182,17 +102,16 @@ namespace NWN.Systems
         successPercent = Math.Clamp(successPercent, minSuccessPercent, maxSuccessPercent);
 
         player.menu.Clear();
-        player.menu.title = $"Desirez vous ajouter l'enchantement {enchantmentName} a l'item {NWScript.GetName(oItem)} ?" +
-          $"Il vous en coutera {cost} POS." +
-          $"Vous avez {successPercent * 100}% de chance de reussite." +
+        player.menu.title = $"Desirez vous ajouter l'enchantement {enchantmentName} a l'item {NWScript.GetName(oItem)} ? " +
+          $"Il vous en coutera {cost} POS. " +
+          $"Vous avez {successPercent * 100}% de chance de reussite. " +
           $"En cas d'échec, l'item sera detruit de facon permanente.";
-        player.menu.choices.Add(("Confirmer.", () => HandleConfirm(ip, cost, successPercent)));
-        player.menu.choices.Add(("Retour.", () => DrawMenu()));
-        player.menu.choices.Add(("Fermer.", () => player.menu.Close()));
+        player.menu.choices.Add(("Confirmer", () => HandleConfirm(player, oItem, ip, cost, successPercent)));
+        player.menu.choices.Add(("Retour", () => DrawMenu(player, oItem)));
         player.menu.Draw();
       }
 
-      private void HandleConfirm(ItemProperty ip, int cost, int successPercent)
+      private void HandleConfirm(PlayerSystem.Player player, uint oItem, ItemProperty ip, int cost, int successPercent)
       {
         if (NWScript.GetGold(player.oid) <= cost)
         {
@@ -214,6 +133,165 @@ namespace NWN.Systems
           NWScript.DestroyObject(oItem);
           NWScript.ApplyEffectToObject(NWScript.DURATION_TYPE_INSTANT, NWScript.EffectVisualEffect(NWScript.VFX_IMP_FLAME_M), oItem);
         }
+      }
+
+      private void AddAttackBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        var currentAttackBonus = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ATTACK_BONUS);
+        if (currentAttackBonus < maxAttackBonus)
+        {
+          player.menu.choices.Add((
+            "Bonus d'attaque",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAttackBonus(currentAttackBonus + 1),
+              $"Bonus d'attaque +{currentAttackBonus + 1}"
+            )
+          ));
+        }
+      }
+
+      private void AddACBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        var currentAC = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_AC_BONUS);
+        if (currentAC < maxACBonus)
+        {
+          player.menu.choices.Add((
+            "Bonus d'armure",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyACBonus(currentAC + 1),
+              $"Bonus d'armure +{currentAC + 1}"
+            )
+          ));
+        }
+      }
+
+      private void AddAbilityBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        player.menu.choices.Add((
+          "Bonus de caracteristique",
+          () => DrawAbilityBonusPage(player, oItem)
+        ));
+      }
+
+      private void DrawAbilityBonusPage(PlayerSystem.Player player, uint oItem)
+      {
+        player.menu.Clear();
+        player.menu.title = "Choisissez une caracteristique";
+
+        // STR
+        var currentSTR = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_STRENGTH);
+        if (currentSTR < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Force",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_STRENGTH, currentSTR + 1),
+              $"Bonus de force +{currentSTR + 1}"
+            )
+          ));
+        }
+
+        // DEX
+        var currentDEX = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_DEXTERITY);
+        if (currentDEX < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Dexterite",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_DEXTERITY, currentDEX + 1),
+              $"Bonus de dexterite +{currentDEX + 1}"
+            )
+          ));
+        }
+
+        // CON
+        var currentCON = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_CONSTITUTION);
+        if (currentCON < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Constitution",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_CONSTITUTION, currentCON + 1),
+              $"Bonus de constitution +{currentCON + 1}"
+            )
+          ));
+        }
+
+        // INT
+        var currentINT = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_INTELLIGENCE);
+        if (currentINT < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Intelligence",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_INTELLIGENCE, currentINT + 1),
+              $"Bonus d'intelligence +{currentINT + 1}"
+            )
+          ));
+        }
+
+        // CHA
+        var currentCHA = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_CHARISMA);
+        if (currentCHA < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Charisme",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_CHARISMA, currentCHA + 1),
+              $"Bonus de charisme +{currentCHA + 1}"
+            )
+          ));
+        }
+
+        // WIS
+        var currentWIS = ItemUtils.GetItemPropertyBonus(oItem, NWScript.ITEM_PROPERTY_ABILITY_BONUS, NWScript.ABILITY_WISDOM);
+        if (currentWIS < maxAbilityBonus)
+        {
+          player.menu.choices.Add((
+            "Sagesse",
+            () => DrawCostPage(
+              player,
+              oItem,
+              NWScript.ItemPropertyAbilityBonus(NWScript.ABILITY_WISDOM, currentWIS + 1),
+              $"Bonus de sagesse +{currentWIS + 1}"
+            )
+          ));
+        }
+
+        player.menu.choices.Add((
+          "Retour",
+          () => DrawMenu(player, oItem)
+        ));
+        player.menu.Draw();
+      }
+
+      private void AddDamageBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        // TODO
+      }
+
+      private void AddSavingThrowBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        // TODO
+      }
+
+      private void AddRegenBonusToMenu(PlayerSystem.Player player, uint oItem)
+      {
+        // TODO
       }
     }
   }
--- Systems/EnchantmentBasin/EnchantmentBasinSystem.cs
+++ Systems/EnchantmentBasin/EnchantmentBasinSystem.cs
@@ -4,17 +4,62 @@ namespace NWN.Systems
 {
   public static partial class EnchantmentBasinSystem
   {
+    private static EnchantmentBasin GetEnchantmentBasinFromTag(string tag)
+    {
+      switch (tag)
+      {
+        default:
+          return new EnchantmentBasin(
+            minSuccessPercent: 5,
+            maxSuccessPercent: 95,
+            maxAttackBonus: 3,
+            maxACBonus: 3,
+            maxAbilityBonus: 3,
+            maxDamageBonus: NWScript.DAMAGE_BONUS_1d12,
+            maxSavingThrowBonus: 3,
+            maxRegenBonus: 2
+          );
+
+        case "enchantment_basin_expensive":
+          return new EnchantmentBasin(
+            costRate: 2,
+            minSuccessPercent: 5,
+            maxSuccessPercent: 95,
+            maxCostRateForSuccessRateMin: 2000000,
+            maxAttackBonus: 5,
+            maxACBonus: 5,
+            maxAbilityBonus: 6,
+            maxDamageBonus: NWScript.DAMAGE_BONUS_1d8,
+            maxSavingThrowBonus: 4,
+            maxRegenBonus: 4
+          );
+      }
+    }
+
     public static int HandleClose(uint oid)
     {
       var oPC = NWScript.GetLastClosedBy();
       PlayerSystem.Player player;
 
-      if (!PlayerSystem.Players.TryGetValue(oPC, out player)) return 0;
+      if (!PlayerSystem.Players.TryGetValue(oPC, out player))
+      {
+        NWScript.SendMessageToPC(oPC, "Player is not valid.");
+        return 0;
+      }
 
-      var oItem = NWScript.GetFirstItemInInventory();
+      var oItem = NWScript.GetFirstItemInInventory(oid);
 
-      if (oItem == NWScript.OBJECT_INVALID) return 0;
-      if (!ItemUtils.IsEquipable(oItem)) return 0;
+      if (oItem == NWScript.OBJECT_INVALID)
+      {
+        NWScript.SendMessageToPC(oPC, "Item is not valid.");
+        return 0;
+      }
+
+      if (!ItemUtils.IsEquipable(oItem))
+      {
+        NWScript.SendMessageToPC(oPC, "Item is not equipable.");
+        return 0;
+      }
 
       if (NWScript.GetPlotFlag(oItem) == 1)
       {
@@ -22,15 +67,15 @@ namespace NWN.Systems
         return 0;
       }
 
-      var oSecondItem = NWScript.GetNextItemInInventory();
+      var oSecondItem = NWScript.GetNextItemInInventory(oid);
       if (oSecondItem != NWScript.OBJECT_INVALID)
       {
         NWScript.SendMessageToPC(oPC, "Invalid number of items.");
         return 0;
       }
 
-      var enchantmentBasin = new EnchantmentBasin(player, oItem);
-      enchantmentBasin.DrawMenu();
+      var tag = NWScript.GetTag(oItem);
+      GetEnchantmentBasinFromTag(tag).DrawMenu(player, oItem);
 
       return 0;
     }
