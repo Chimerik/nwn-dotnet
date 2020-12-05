151cbaced16a08bbbf8db3feac0f5a679bba6043
Chim <chimaera.no.saki@gmail.com>
2020-12-05 16:56:12 +0100

    Ajout de subtilité pour la commande permettant de s'asseoir partout

---
+++ Systems/CommandSystem/Commands/RotateCommand.cs
@@ -0,0 +1,22 @@
+﻿using NWN.Core;
+
+namespace NWN.Systems
+{
+  public static partial class CommandSystem
+  {
+    private static void ExecuteRotateCommand(ChatSystem.Context ctx, Options.Result options)
+    {
+      if (((string)options.positional[0]).Length != 0)
+      {
+        float value;
+        if (float.TryParse((string)options.positional[0], out value))
+        {
+          NWScript.SetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_ROTATE_X, NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_ROTATE_X) + value);
+          return;
+        }
+      }
+
+      NWScript.SetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_ROTATE_X, NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_ROTATE_X) + 20.0f);
+    }
+  }
+}
--- Systems/CommandSystem/Commands/UpCommand.cs
+++ Systems/CommandSystem/Commands/UpCommand.cs
@@ -6,17 +6,27 @@ namespace NWN.Systems
   {
     private static void ExecuteUpCommand(ChatSystem.Context ctx, Options.Result options)
     {
+      float zPos;
       if (((string)options.positional[0]).Length != 0)
       {
         float value;
         if (float.TryParse((string)options.positional[0], out value))
         {
           NWScript.SetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z, NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z) + value);
+
+          zPos = NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z);
+          if (zPos < 0 || zPos > 5)
+            Utils.LogMessageToDMs($"SIT COMMAND - Player {NWScript.GetName(ctx.oSender)} - Z translation = {zPos}");
+          
           return;
         }
       }
 
       NWScript.SetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z, NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z) + 0.1f);
+
+      zPos = NWScript.GetObjectVisualTransform(ctx.oSender, NWScript.OBJECT_VISUAL_TRANSFORM_TRANSLATE_Z);
+      if (zPos < 0 || zPos > 5)
+        Utils.LogMessageToDMs($"SIT COMMAND - Player {NWScript.GetName(ctx.oSender)} - Z translation = {zPos}");
     }
   }
 }
