3855382933ccfd1fad42d449aa1d3e33f4416b63
Chim <chimaera.no.saki@gmail.com>
2021-01-02 23:40:38 +0100

    Ajouts commandes Discord Dev & Backlog pour détailler les tâches

---
+++ Systems/BotSystem/AsyncCommands/DisplayBacklogInfoCommand.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace NWN.Systems
+{
+  public static partial class BotSystem
+  {
+    public static async Task ExecuteDisplayBacklogInfoCommand(SocketCommandContext context)
+    {
+      await context.Channel.SendMessageAsync("Backlog de projets en attente\n" +
+        "    - Backlog\n" +
+        "    - Craft menuiserie / ébeniste\n" +
+        "    - Craft tannerie\n" +
+        "    - Boutiques PJs\n" +
+        "    - Malus de mort\n" +
+        "    - Système de durabilité des objets\n" +
+        "    - Système de lycanthropie(et autres malédictions)\n" +
+        "    - Métier recherche d'enchantement\n" +
+        "    - Systéme d'amélioration des items craftés (application des enchantements)\n" +
+        "    - Système de remboursement de la dette en cas de non solvabilité\n" +
+        "    - Système de repos\n" +
+        "    - Système de transport de matières premières\n" +
+        "    - Métier Craft parchemins\n" +
+        "    - Métier Craft potions\n" +
+        "    - Métier Craft baguettes\n" +
+        "    - Métier Craft meubles persistants(via menuiserie / ébenisterie)\n" +
+        "    - Système de messagerie + écriture de livre\n" +
+        "    - Métier 'Professeur'\n" +
+        "    - Système de déguisement\n" +
+        "    - Renommer ses invocations\n" +
+        "    - Faire parler ses invocations\n" +
+        "    - Système de faim / soif(apporte bonus apprentissage / craft)\n" +
+        "    - Système de repas d'auberge/taverne (différents coûts/qualité/bonus)\n" +
+        "    - Métier cuisinier\n" +
+        "    - Système d'amélioration des outils de craft\n" +
+        "    - Système de couleurs de chat ?\n" +
+        "    - Système de factions\n" +
+        "    - Coloration spécifique des emotes(texte rp entre * *)\n" +
+        "    - Permettre destruction de cadavres PJs\n" +
+        "    - Canal groupe rp(autres pjs à portée d'oreille peuvent entendre)\n" +
+        "    - Système de personnalisation d'apparence de vêtements (seul le crafteur initial pourra modifier)\n" +
+        "    - Cooldowns sur dons activables(renversement, furtif, etc)\n" +
+        "    - Amélioration minage(différentes qualités de filon identifiables par prospection)\n" +
+        "    - Amélioration craft d'objets (ajouter talent qui donne % chance de réduire poids, augmenter durabilité et nb slots enchantements)\n" +
+        "    - Talent épique permettant aux archers de ne pas subir d'AOO\n" +
+        "    - Système 'savoir rp' (livres à apprendre)\n" +
+        "    - Talent permettant d'ajouter un sort à une attaque\n" +
+        "    - Système de persistance des sorts\n" +
+        "    - Réfléchir à que faire de Utilisation des Objets Magiques\n" +
+        "    - Réfléchir à que faire de Persuasion\n" +
+        "    - Réfléchir à que faire d'Equitation (et des montures en général)\n\n"
+        );
+    }
+  }
+}
---
+++ Systems/BotSystem/AsyncCommands/DisplayDevInfoCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace NWN.Systems
+{
+  public static partial class BotSystem
+  {
+    public static async Task ExecuteDisplayDevInfoCommand(SocketCommandContext context)
+    {
+      await context.Channel.SendMessageAsync("Développements et tâches en cours\n" +
+        "    - Collecte de bois\n" +
+        "    - Collecte de peaux / cuirs\n" +
+        "    - Système de jukebox(pnjs bardes en taverne jouent diverses chansons.Peut apporter un bonus supplémentaire si un joueur paye)\n" +
+        "    - Maps ?\n" +
+        "    - Icônes pour dons customs\n" +
+        "    - Ambiance sonore du module(musiques et sons)\n" +
+        "    - Images de chargement des zones\n" +
+        "    - Réfléchir à rencontres / combats / boss / donjons intéressants\n" +
+        "    - Ecrire les descriptions de choses non décrites en jeu\n" +
+        "    - Création de pnjs + background + description pour animation\n\n"
+        );
+    }
+  }
+}
