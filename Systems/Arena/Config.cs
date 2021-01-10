a9aed13838bab82472b1401461b773db9b2bc0bb
RGR <rg.robingranger@gmail.com>
2021-01-10 19:18:38 +0100

    feature(Arena) : ajoute le menu d'inscription à l'arene

---
+++ Systems/Arena/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace NWN.Systems.Arena
+{
+  public static class Config
+  {
+    public enum Difficulty
+    {
+      Level1,
+      Level2,
+      Level3,
+      Level4,
+      Level5,
+    }
+  }
+}
---
+++ Systems/Arena/Menu.cs
@@ -0,0 +1,85 @@
+﻿using static NWN.Systems.Arena.Config;
+using static NWN.Systems.PlayerSystem;
+
+namespace NWN.Systems.Arena
+{
+  public static class Menu
+  {
+    public static void Draw(Player player)
+    {
+      player.menu.Clear();
+      player.menu.title = "Bienvenue dans l'arene de Similisse ! Que puis-je faire pour vous aujourd'hui ?";
+      player.menu.choices.Add((
+        "M'inscrire pour participer aux prochains combats",
+        () => DrawSubcribePage(player)
+      ));
+      player.menu.choices.Add((
+        "Depenser mes points de victoire pour acheter des récompenses",
+        () => DrawRewardPage(player)
+      ));
+      player.menu.choices.Add((
+        "Voir la liste des meilleurs combattants",
+        () => DrawHighscoresPage(player)
+      ));
+
+      player.menu.Draw();
+    }
+
+    private static void DrawSubcribePage(PlayerSystem.Player player)
+    {
+      player.menu.Clear();
+      player.menu.title = "Veuillez choisir votre niveau de difficulté";
+      player.menu.choices.Add((
+         "1. C'est ma première fois, ne tapez pas trop fort s'il vous plaît !",
+         () => DrawConfirmPage(player, Difficulty.Level1)
+      ));
+
+      player.menu.Draw();
+    }
+
+    private static void DrawConfirmPage(Player player, Difficulty difficulty)
+    {
+      player.menu.Clear();
+      player.menu.title = $"Très bien, il vous en coûtera {Utils.GetGoldEntryCost(difficulty)} PO. Etes-vous prêt a rentrer dans l'arene ?";
+      player.menu.choices.Add((
+        "Absolument, je suis prêt à en découdre !",
+        () => HandleConfirm(player, difficulty)
+      ));
+      player.menu.choices.Add((
+        "Non, peut-être une autre fois.",
+        () => player.menu.Close()
+      ));
+
+      player.menu.Draw();
+    }
+
+    private static void HandleConfirm(Player player, Difficulty difficulty)
+    {
+      player.PayOrBorrowGold(Utils.GetGoldEntryCost(difficulty));
+
+      // TODO TP le PJ dans la zone instanciée
+    }
+
+    private static void DrawRewardPage(PlayerSystem.Player player)
+    {
+      player.menu.Clear();
+      player.menu.title = "Vous avez actuellement xxx points de victoire disponible. Voici la liste des récompenses disponibles :";
+      player.menu.choices.Add((
+        "Retour",
+        () => Draw(player)
+      ));
+      player.menu.Draw();
+    }
+
+    private static void DrawHighscoresPage(PlayerSystem.Player player)
+    {
+      player.menu.Clear();
+      player.menu.title = "Voici la liste de nos meilleurs champions :";
+      player.menu.choices.Add((
+        "Retour",
+        () => Draw(player)
+      ));
+      player.menu.Draw();
+    }
+  }
+}
