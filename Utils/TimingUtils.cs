34e135c494bf9afd8f18428dca2bb1d22ff36575
RGR <rg.robingranger@gmail.com>
2021-01-17 22:18:41 +0100

    feature(TimingUtils) : ajoute une fonction Debounce

---
+++ Utils/TimingUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NWN.Core;
+
+namespace NWN
+{
+  public static class TimingUtils
+  {
+    public static Action Debounce(Action action, float delay)
+    {
+      var cancelDelay = new List<bool>();
+      return () =>
+      {
+        if (cancelDelay.Count != 0)
+        {
+          cancelDelay[cancelDelay.Count - 1] = true;
+        }
+        cancelDelay.Add(false);
+        var currentIndex = cancelDelay.Count - 1;
+
+        NWScript.DelayCommand(delay, () => { 
+          if (!cancelDelay[currentIndex])
+          {
+            action();
+          }
+        });
+      };
+    }
+  }
+}
