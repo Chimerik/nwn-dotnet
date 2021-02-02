18e7fb2066f8c72d39bd49d38f21d841a259e518
RGR <rg.robingranger@gmail.com>
2021-02-02 21:00:14 +0100

    evolution(Debounce) : ajoute une paramètre à la callback

--- Utils/TimingUtils.cs
+++ Utils/TimingUtils.cs
@@ -6,11 +6,11 @@ namespace NWN
 {
   public static class TimingUtils
   {
-    public static Action Debounce(Action action, float delay)
+    public static Action<T1> Debounce<T1>(Action<T1> action, float delay)
     {
       var cancelDelay = new Dictionary<int, bool>();
       int nextId = 0;
-      return () =>
+      return (T1) =>
       {
         if (cancelDelay.Count != 0)
         {
@@ -21,13 +21,13 @@ namespace NWN
         // Copy id before incrementation for use in the DelayCommand
         var currentId = nextId;
 
-        NWScript.DelayCommand(delay, () => {
+        NWScript.AssignCommand(NWScript.GetModule(), () => NWScript.DelayCommand(delay, () => {
           if (!cancelDelay[currentId])
           {
-            action();
+            action(T1);
           }
           cancelDelay.Remove(currentId);
-        });
+        }));
 
         nextId++;
       };
